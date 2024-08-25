369c63d97ec74fe3e8f88a0ac5a529d90c9b4340
Michal <killbwoy@gmail.com>
2024-08-25 15:27:56 +0200

    aktualizacja connect.php
    aktualizacja connect.php

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
 
-	$host = "localhost";
-	$db_user = "root";
-	$db_password = "";
-	$db_name = "budget";
+	$host = "finanse.michal-kuta.profesjonalnyprogramista.pl.mysql.dhosting.pl";
+	$db_user = "uji7ei_finansem";
+	$db_password = "Ci4oecha2koo";
+	$db_name = "aepah9_finansem";
 	
 
 ?>
